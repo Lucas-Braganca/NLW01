be8f8bc9e2f3e69523cfb52c89b8720dd1a22624
Lucas <lucas.braganca@clubpetro.com>
2020-08-08 17:52:23 -0300

    Frontend and backend

---
+++ server/commands.md
@@ -0,0 +1,5 @@
+#Rodar a migration com o Knex:
+ - npx knex migrate:latest --knexfile knexfile.ts migrate:latest
+
+#Iniciar o server:
+ - npx ts-node src/server.ts
---
+++ server/src/database/connection.ts
@@ -0,0 +1,16 @@
+import knex from 'knex';
+import path from 'path';
+
+const connection = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite'),
+    },
+    useNullAsDefault: true,
+    pool: {
+        max: 7,
+        min: 3,
+    }
+});
+
+export default connection;
